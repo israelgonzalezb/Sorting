dcf6569636b9792e9809d7b81a1330e671fe0441
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-01-19 15:31:27 -0800

    Selection sort completed, need to refactor to clean up code

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,11 +4,24 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        print("________")
+        print(arr[i])
+        print("________")
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        for j in range(cur_index + 1, len(arr)):
+            current_item = arr[i]
+            compare_item = arr[j]
+            print(arr[j])
+            if arr[i] > arr[j]:
+                print(f"{arr[i]} is bigger than {arr[j]}")
+                arr[i] = compare_item
+                arr[j] = current_item
+            elif arr[i] < arr[j]:
+                print(f"{arr[i]} is smaller than {arr[j]}")
+            else:
+                print(f"{arr[i]} is the same as {arr[j]}")
              
-
-
 
         # TO-DO: swap
 
@@ -28,3 +41,5 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+print(selection_sort([1,0,0,3,5,2,0,4]))
