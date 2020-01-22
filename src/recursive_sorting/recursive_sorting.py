93ac431ef9871efdb6c53c9d02bd2125c2ab63f3
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-01-21 20:15:47 -0800

    Working on merge_sort and merge function connection

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -36,23 +36,29 @@ def merge( arrA, arrB ):
     # 1. Create an array merged_arr of size n1 + n2.
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
+    print(f"Our array of elements of size n1 + n2 is {merged_arr} and has {len(merged_arr)} elements")
     # TO-DO
     # 2. Copy all n1 elements of arrA to merged_arr
     for i in range(len(arrA)):
         merged_arr[i] = arrA[i]
-    print(merged_arr)
+    print(f"Our merged array with items from the first array is {merged_arr}")
+    # Track where arrB element was last inserted so we can skip forward
+    last_insert = 0
     for k in range(len(arrB)):
-        print(k)
-        for j in range(len(merged_arr)):
-            if j == len(merged_arr)-1:
-                print(f"Comparing {merged_arr[j]} and {arrB[k]}")
-                merged_arr.insert(j,arrB[k])
-                merged_arr.pop()
+        print(f"For arrB {arrB}, currently at index {k} of {len(arrB)} elements. The element is {arrB[k]}.")
+        for j in range(last_insert,len(arrA)+k):
+            print(f"  For merged_arr {merged_arr}, currently at index {j} of {range(last_insert,len(arrA)+k)}. The element is {merged_arr[j]}.")
             if arrB[k] < merged_arr[j]:
-                print(f"Comparing {merged_arr[j]} and {arrB[k]}")
+                print(f"    {arrB[k]} is less than {merged_arr[j]}, so insert {arrB[k]} into merged_arr, remove the last element of merged_arr and break out of this loop. We inserted at {j} so we'll set the last_insert index to {j+1}")
+                last_insert = j + 1
                 merged_arr.insert(j,arrB[k])
                 merged_arr.pop()
                 break
+            if j + 1 == len(arrA)+k:
+                print(f"Sliced merged_arr should only contain {arrA}: {merged_arr[0:j+1]}")
+                merged_arr = merged_arr[0:j+1] + arrB
+                print(merged_arr)
+                return merged_arr
 
     print(merged_arr)
 
@@ -64,8 +70,8 @@ def merge( arrA, arrB ):
 def merge_sort( arr ):
     # TO-DO
     if len(arr) == 1:
-        #print(first_half, second_half)
         print(arr)
+        print("there is one item in this array")
         return arr;
     else:
         first_half = []
@@ -81,13 +87,14 @@ def merge_sort( arr ):
             for i in range((len(arr)//2)+1,len(arr)):
                 second_half.append(arr[i])
 
-        print(first_half,second_half)
+        print(f"The two halves are: {first_half},{second_half}")
+        #merged_arr = merge(first_half, second_half)
+        #print(f"The merge function returned: {merged_arr}")
         merge_sort(first_half)
         merge_sort(second_half)
-        return arr
 
-#merge_sort([38,27,43,3,9,82,10])
-merge([35,38,45],[25,26,54])
+merge_sort([38,27,43,3,9,82,10])
+#merge([35,38,45],[78,79,80])
 
 """
 # STRETCH: implement an in-place merge sort algorithm
